d96ddf0af339880a0ce3ad72c72aceb957359324
warrior14 <ironaddicts14@gmail.com>
2021-09-14 11:43:08 -0500

    refactored JournalManager.cs and MainMenuManager.cs

--- TabloidCLI/UserInterfaceManagers/JournalManager.cs
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -1,10 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
-    class JournalManager
+    public class JournalManager : IUserInterfaceManager
     {
+        private readonly IUserInterfaceManager _parentUI;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Tag Menu");
+            Console.WriteLine(" 1) List Journals");
+            Console.WriteLine(" 2) Add Journal");
+            Console.WriteLine(" 3) Edit Journal");
+            Console.WriteLine(" 4) Remove Journal");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "2":
+                    Add();
+                    return this;
+                case "3":
+                    Edit();
+                    return this;
+                case "4":
+                    Remove();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Remove()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
