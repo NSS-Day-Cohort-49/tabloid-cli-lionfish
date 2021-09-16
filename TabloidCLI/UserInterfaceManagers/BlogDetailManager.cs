ce33c055cb1dbe183e68c46e312ca330db2666d6
abbeyroyse13 <heatherruan126@gmail.com>
2021-09-16 10:36:14 -0500

    saving

--- TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
+++ TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
@@ -7,6 +7,15 @@ namespace TabloidCLI.UserInterfaceManagers
 {
     internal class BlogDetailManager : IUserInterfaceManager
     {
+        private IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepository;
+        private PostRepository _postRepository;
+        private TagRepository _tagRepository;
+        private int _authorId;
 
+        public BlogDetailManager(IUserInterfaceManager) parentUI, string connectionString, int blogId) 
+            {
+                
+            }
     }
 }
